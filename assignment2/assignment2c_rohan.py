7168af874332eae6582adf6d524ba96ee43b5eed
rohanzonneveld <rohanzonneveld@gmail.com>
2023-11-17 11:59:31 +0100

    update 2c

--- assignment2/assignment2c_rohan.py
+++ assignment2/assignment2c_rohan.py
@@ -1,14 +1,11 @@
 import numpy as np
 
-distribtion = np.zeros(91)
-distribtion[0] = 1
+distribution = np.zeros(91)
+distribution[0] = 1
 failure_probs = np.arange(0.10, 1.01, 0.01)
 
 for _ in range(100):
-    pi_1_new = np.sum(distribtion * failure_probs)
-    pi_rest_new = distribtion[:-1] * (1 - failure_probs[:-1])
-    distribtion[0] = pi_1_new
-    distribtion[1:] = pi_rest_new
+    distribution = np.concatenate(([np.sum(distribution * failure_probs)], distribution[:-1] * (1 - failure_probs[:-1])))
 
-print(distribtion)
-print(np.sum(distribtion))
+print(distribution)
+print(np.sum(distribution))

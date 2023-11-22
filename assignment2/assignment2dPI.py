93dfa658208d7fdd6a8768c64ee44d385b441df9
rohanzonneveld <rohanzonneveld@gmail.com>
2023-11-22 15:14:06 +0100

    Policy Iteration

---
+++ assignment2/assignment2dPI.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+# construct state vector
+x = np.arange(0.1, 1.01, 0.01) 
+
+# construct transition matrices for both actions
+P1 = np.zeros((91,91))
+P2 = np.zeros((91,91))
+P2[:,0] = 1
+for i in range(90):
+    P1[i,0] = x[i]
+    P1[i,i+1] = 1 - x[i]
+P1[-1,0] = 1
+
+
+# Policy Iteration
+# Step 1: Set policy alpha
+alpha = [1]*91
+
+# repeat 2 and 3 until convergence
+while True:
+    # Step 2: Solve Poisson equation
+    # Construct vector r and matrix P
+    r = np.zeros(91)
+    P = np.zeros((91,91))
+    for i in range(91):
+        if alpha[i] == 1:
+            r[i] = x[i]
+            P[i,:] = P1[i,:]
+        elif alpha[i] == 2:
+            r[i] = 0.6
+            P[i,:] = P2[i,:]
+
+    # Construct matrix A and vector b
+    # V + phi = r + P*V
+    A = np.eye(91) - P
+    A[:,0] = 1 # change V(0) to phi
+    b = r
+
+    # Solve for V and phi
+    V = np.linalg.solve(A, b)
+    V = V-V[0]
+
+    # Step 3: Improve policy
+    Q = np.zeros((91,2))
+    Q[:,0] = x + np.dot(P1,V)
+    Q[:,1] = 0.6 + np.dot(P2,V)
+    alpha_new = np.argmin(Q,axis=1) + 1
+
+    # Check for convergence
+    if np.all(alpha_new == alpha):
+        break
+    else:
+        alpha = alpha_new
+
+print(alpha)
+
+
+
+  
